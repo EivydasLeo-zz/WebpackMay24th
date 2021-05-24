bd051a534aa03ea3c6ea1b80dce72f97b38c1621
Your Name <you@example.com>
2021-05-24 13:38:47 +0300

    babel + source map

--- src/js/greet.js
+++ src/js/greet.js
@@ -1,3 +1,5 @@
 export default function greet(say) {
   return "Bob said" + " " + say;
 }
+
+console.log("as gyvenu greet js faile");
--- webpack.dev.js
+++ webpack.dev.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
+  devtool: "source-map", // galima matyti is kurio failo koks kodas atejo
   entry: {
     // kuri faila paims webpack kaip pagrindini
     main: path.resolve(__dirname, "./src/app.js"),
@@ -14,9 +15,21 @@ module.exports = {
   },
   module: {
     rules: [
+      // css loader
       {
         test: /\.css$/i, // pritaikom .css failam
         use: ["style-loader", "css-loader"], // uzkraunam css
+      },
+      // babel loader
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
       },
     ],
   },
