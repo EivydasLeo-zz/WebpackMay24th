9fc54627b5d3b9adb94924c17652e16a4cafb66e
Your Name <you@example.com>
2021-05-24 15:01:38 +0300

    image minimizer plugin

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,6 @@
 const path = require("path"); // node modulis dirbti su keliais iki failu
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-
+const HtmlWebpackPlugin = require("html-webpack-plugin"); // html generavimo
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
 module.exports = {
   mode: "development",
   devtool: "source-map", // galima matyti is kurio failo koks kodas atejo
@@ -45,6 +45,18 @@ module.exports = {
     ],
   },
   plugins: [
+    new ImageMinimizerPlugin({
+      minimizerOptions: {
+        // Lossless optimization with custom option
+        // Feel free to experiment with options for better result for you
+        plugins: [
+          ["gifsicle", { interlaced: true }],
+          ["mozjpeg", { quality: 50 }],
+          ["pngquant", { quality: [0.5, 0.7] }],
+          ["svgo"],
+        ],
+      },
+    }),
     new HtmlWebpackPlugin({
       template: "./src/html/template.html",
       templateParameters: {
